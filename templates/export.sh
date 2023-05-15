3beeaee1c3e28fea968b99e8a84f97b150b3386a
Oliver Filla <ocl@hotmail.de>
2023-05-15 13:28:06 +0200

    structure

--- templates/export.sh
+++ templates/export.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cat ??_*.md > "Algorithmen & Datenstrukturen.md" || exit $?
+cat Mitschriften/??_*.md > "Algorithmen & Datenstrukturen.md" || exit $?
 pandoc --defaults templates/defaults.yml --top-level-division=chapter $@ || exit $?
